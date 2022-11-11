f577c022919a439a146273d2475aa6261f62a64b
Silvestre525 <silvestrevillar02@gmail.com>
2022-11-11 16:33:41 -0300

    Implemente Multer y validaciones del backend

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const {validationResult} = require('express-validator')
 
 const controller = {
     index:(req,res) =>{
@@ -17,10 +18,15 @@ const controller = {
         return res.render('detalle');
     },
     crearUsuario:(req,res) =>{
-        return res.send({
-            body: req.body,
-            file: req.file
-        });
+        const resultValidation = validationResult(req);
+        if(resultValidation.errors.length > 0){   //si mi array es mayor a 0, osea si tiene mas de 1 error se activa el if
+            return res.render('register',{  //Voy a devolver mi vista de regitro y ahi con el mapped lo tranformo en un objeto literarl
+                errors:resultValidation.mapped(),
+                oldData:req.body  //Lo que quedo en el req.body ates de darle registrar
+            });
+        }
+
+        return res.send('Ok sin errores, aca podrias añadir una vista');
     }
 }
 
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const controller = require("../controller/controller");
 const routes = express.Router();
 const path = require('path');
+
+//Validaciones del Backend
+const {body} = require('express-validator');
+
 //Multer
 const multer = require('multer');
 const storage = multer.diskStorage({
@@ -16,12 +20,37 @@ const storage = multer.diskStorage({
 
 const uploadfile = multer(storage);
 
+//Validaciones del Backend
+const validations = [
+    body('nombre').notEmpty().withMessage('No puedes dejar este campo vacio'),
+    body('apellido').notEmpty().withMessage('No puedes dejar este campo vacio'),
+    body('email')
+    .notEmpty().withMessage('No puedes dejar este campo vacio').bail()
+    .isEmail().withMessage('Debes poner un formato valido de correo'),  //bali sirve para hacer 2 validaciones en un mismo campo
+    body('password').notEmpty().withMessage('No puedes dejar este campo vacio'),
+    body('avatar').custom((value,{req})=>{
+        let file = req.file;
+        let accepted = ['.jpg','.JPG','.gif','.GIF','.png','.PNG'];
+        
+        if(!file){
+            throw new Error ('debe insertar una imagen');
+        }else{
+            let fileExtension = path.extname(file.originalname);
+            if(!accepted.includes(fileExtension)){
+                throw new Error (`Las extenciones permitidas son ${accepted.join(',')}`);
+            }  //Esto es una validacion custom la cual tenemos que inventar ya que no esta en express validator
+        }
+        return true;
+    })
+];
+
 /* Indico cuando me mostrará el home de mi página */
 routes.get ('/',controller.index);
 
 /* Indico cuando se mostrará el register de mi página */
 routes.get ('/register',controller.registro);
-routes.post('/register',uploadfile.single('avatar'),controller.crearUsuario);
+//Procesar registro
+routes.post('/register',uploadfile.single('avatar'),validations,controller.crearUsuario);
 
 /* Indico cuando se mostrará el login de mi página */
 routes.get ('/login',controller.login);
